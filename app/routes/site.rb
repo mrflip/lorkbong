5f58318e869ec86c9f86ff2f9c19f0f13addc24d
Philip (flip) Kromer <flip@infochimps.org>
2010-11-02 02:15:35 -0500

    Adding EMR scripts

--- app/routes/site.rb
+++ app/routes/site.rb
@@ -11,8 +11,6 @@ class Main
 
   get '/emr/list' do
     [ '<pre>',
-      Dir[::ROOT_DIR+'/vendor/*'],
-      `ls -l #{::ROOT_DIR}/vendor/*/*`,
       %Q{ #{::ROOT_DIR}/vendor/elastic-mapreduce/elastic-mapreduce -a $AWS_ACCESS_KEY_ID  -p $AWS_SECRET_ACCESS_KEY --list },
       %x{ ruby #{::ROOT_DIR}/vendor/elastic-mapreduce/elastic-mapreduce -a $AWS_ACCESS_KEY_ID  -p $AWS_SECRET_ACCESS_KEY --list },
       ENV['AWS_ACCESS_KEY_ID'],
