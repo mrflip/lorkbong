9bd13182dcadbf051b35fa59223554aff7deea31
Philip (flip) Kromer <flip@infochimps.org>
2010-11-02 05:45:49 -0500

    Making script simpler

--- tasks/adjlist_degree.rb
+++ tasks/adjlist_degree.rb
@@ -1,34 +1,34 @@
 #!/usr/bin/env ruby
 ::ROOT_DIR = File.expand_path(File.dirname(__FILE__)+'/..') unless defined? ::ROOT_DIR
-Dir[::ROOT_DIR+'/vendor/**/lib'].each{|dir| p dir ; $: << dir }
+Dir[::ROOT_DIR+'/vendor/**/lib'].each{|dir| $: << dir }
 EMR_CONFIG_DIR = ::ROOT_DIR + '/config'
-p $:
 #
 require 'rubygems'
 require 'wukong'
 require 'wukong/script/emr_command'
 
+#
+# Takes a graph in adjacency list form (node id followed by id of each out-neighbor):
+#
+#   node_id: nbr_id nbr_id nbr_id nbr_id
+#
+# Emits the node_id and its out-degree -- the cound of all its neighbors
+#
 class AdjacencyListToDegree < Wukong::Streamer::RecordStreamer
-
+  # pull off the first field as the node id, the rest as neighbors.
   def recordize line
-    id, nbrs = line.split(/\W+/, 2)
+    id, *nbrs = line.split(/\W+/)
     [id, nbrs]
   end
 
+  #
+  # @param id   a string ID for the node
+  # @param nbrs an array of neighbor ids
+  #
   def process id, nbrs
     return if id.blank? || nbrs.blank?
-    yield [id, nbrs.split(/\W+/).count]
+    yield [id, nbrs.count, Time.now.to_i]
   end
 end
 
-class IdentityReducer < Wukong::Streamer::LineStreamer
-  def process *args
-    yield *args
-  end
-end
-
-# Settings[:emr_extra_args] = ["--arg '-D mapred.reduce.tasks=0'"]
-Wukong::Script.new(
-  AdjacencyListToDegree, IdentityReducer,
-  :reduce_tasks => 0, :reuse_jvms => true, :partition_fields => 1, :sort_fields => 2
-  ).run
+Wukong::Script.new( AdjacencyListToDegree, nil ).run
